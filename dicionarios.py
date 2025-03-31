f1eb85ba825f67beb5286727bc4eca42b5696ffa
HeitorAlberto <56478574+HeitorAlberto@users.noreply.github.com>
2025-03-31 10:13:37 -0300

    Update dicionarios.py

--- dicionarios.py
+++ dicionarios.py
@@ -1,8 +1,26 @@
 #Essa estrutura de dados possui chaves e seus respectivos valores
 
-fla = {"Nome": "CR Flamengo", "País": "Brasil", "Estado": "Rio de Janeiro", "Fundação": 1895, "Libertadores": 3}
+x = {"Chave0": "valor0", "Chave1": "valor1"}
 
-print(fla)   # Retorno: {"Nome": "CR Flamengo", "País": "Brasil", "Estado": "Rio de Janeiro", "Fundação": 1895, "Libertadores": 3}
-print(fla["Libertadores"])   # Retorno: 3
+print(x)   # Retorno o dicionário completo
+print(x["chave0"])   # Retorna o valor correspondente da chave informada
+print(x.keys()) # Retorna uma lista com as chaves
+print(x.values()) # Retorna uma lista com os valores
 
-fla["Brasileiros"] = 8    #Atribui uma nova chave ("Brasileiros") e seu respectivo valor (8)
+x["chave2"] = "valor2"    # Se já existir, modifica  /  se não existir, cria um novo par "chave/valor"
+
+y = x.copy()  # y passa a ser uma cópia do dicionário x
+y = dict(x) # da mesma forma y passa a ser uma cópia de x
+
+
+for valor in x.values():
+  print(valor)
+  #O loop acessa e retorna cada valor
+
+for valor in x.keys():
+  print(valor)
+  #O loop acessa e retorna cada chave
+
+for chave, valor in x.items():
+  print(chave, valor)
+  #O loop acessa e retorna cada chave e valor
