3d307975e862e5b7b31684ea58db664ab4e72507
HeitorAlberto <56478574+HeitorAlberto@users.noreply.github.com>
2025-04-24 09:28:52 -0300

    Update numpy-arrays.py

--- numpy-arrays.py
+++ numpy-arrays.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 np.sqrt(9)  #Raiz quadrada de 9: 3
 
 
-x = np.array([1, 2, 3, 4])
+x = np.array([1, 2, 3, 4])  # Arrays numpy conseguem armazenar dados e processá-los de maneira muito mais eficiente (é projetado em linguagem C). 
 
 print(x)              #[1 2 3 4]
 print(type(x))        #<class 'numpy.ndarray'>
@@ -19,14 +20,17 @@ y[0:8:2]    #Acessa os elementos do intervalo de 2 em 2
 
 
 #MATRIZ DE LISTAS
-A = [[3, 2], [5, 3]]
+A = [[3, 2],
+     [5, 3]]
+
 A[0]     #Acessa todos os valores da primeira lista (3, 2)
 A[0][1]    #Acessa a primeira lista mas retorna apenas o seu segundo elemento (2)
 
 
 
 #MATRIZ DE ARRAYS
-B = np.array([[5, 3],[1, -3]])
+B = np.array([[5, 3],
+              [1, -3]])
 
 print(np.ndim(x))     # 2 (duas dimensões)
 
